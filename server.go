951e98a96a0e9613fe0bb85c28b1edaf7b0bb522
yuta4j1 <kascado.ys10@gmail.com>
2020-01-19 12:02:26 +0900

    echoの導入

--- server.go
+++ server.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/labstack/echo"
+)
 
 func main() {
 	fmt.Println("Hello, World!")
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Test")
+	})
+
+	e.Logger.Fatal(e.Start(":8080"))
 }
